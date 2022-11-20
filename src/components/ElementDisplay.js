e211c69fbb24ea907adfbe47d519c9277ddcee37
HanselDsouza <hanseld54@gmail.com>
2022-11-20 17:38:26 +0530

    changes

--- src/components/ElementDisplay.js
+++ src/components/ElementDisplay.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
+import Modal from './Modal'
 
 const ElementDisplay = ({data}) => {
+  const [modalview,setModalView] = useState(false)
+  
   return (
-    <div onClick={()=>console.log(data)} className='text-xl p-4 font-sans capitalize text-gray-100 hover:bg-gray-400 hover:cursor-pointer hover:rounded-sm hover:text-2xl hover:text-yellow-200 hover:font-semibold'>{data}</div>
-  )
+    <>
+    <div onClick={()=>setModalView(!modalview)} className='text-xl p-4 font-sans capitalize text-gray-100 hover:bg-gray-400 hover:cursor-pointer hover:rounded-sm hover:text-2xl hover:text-yellow-200 hover:font-semibold'>{data}</div>
+    {modalview ? <Modal data={data} modalview={modalview} setModalView={setModalView}/> : <></>}
+    </>
+    )
 }
 
 export default ElementDisplay
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React,{useEffect, useState} from "react";
+import "./Modal.css";
+import {IoIosCloseCircleOutline} from 'react-icons/io'
 
-const Modal = () => {
+export default function Modal({data,modalview,setModalView}) {
+
+  const [desc,setDesc] = useState()
+
+  const toggleModal = () => {
+    setModalView(!modalview)
+  };
+
+  if(modalview) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+//   useEffect(()=>{
+//     fetch(`https://api.datamuse.com/words?rel_syn=${data}`).then(res => res.json()).then((data)=>setDesc(data))
+//   },[data])
+
+
   return (
-    <div>Modal</div>
-  )
+    <>
+        <div className="modal ">
+          <div onClick={toggleModal} className="overlay"></div>
+          <div className="modal-content">
+            <div className="divide-y-2 space-y-4 divide-slate-400">
+            <h2 className="text-white">{data}</h2>
+            <p className="p-20 text-white">
+                COMING SOON ...
+            {/* {console.log(desc[0]?.meanings[0]?.definitions[0]?.definition)} */}
+            </p></div>
+            <button className="close-modal text-white text-2xl font-bold hover:text-red-500" onClick={toggleModal}>
+                <IoIosCloseCircleOutline/>
+            </button>
+          </div>
+        </div>
+    </>
+  );
 }
-
-export default Modal
